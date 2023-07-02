2b9b054e260ff18e5b847b66e175911282b5641c
Marialdinha <marialda@bellsouth.net>
2023-07-02 16:30:06 -0500

    Added square, triangle and circle classes

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # SVG-Logo-Maker
+
+## Table of Contents
+  - [Description](#Description)
+ 
+## Description
+  This application requests user input in the terminal in order to generate a logo "logo.svg" file with a SVG image.
+
+  
--- index.js
+++ index.js
@@ -1,36 +1,70 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
-const jest = require('jest');
+// Packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const jest = require("jest");
+const {Circle, Square, Triangle} = require("./lib/shape");
+
+// Global variable
+let Shape;
 
 //Array of questions for user input
 const questions = [
     {
         type: 'input',
-        message: 'Enter your text',
-        name: 'text',
+        name: 'inputText',
+        message: 'Enter your three letters text',
     },
     {
         type: 'input',
-        message: 'Enter your text color',
-        name: 'textColor',
+        name: 'inputTextColor',
+        message: 'Enter your text color keyword or hexadecimal number',
     },
     {
         type: 'list',
-        message: 'Enter your shape',
+        name: 'inputShape',
+        message: 'Choose your shape',
         choices: ["Circle", "Square", "Triangle"],
-        name: 'shape',
     },
     {
         type: 'input',
-        message: 'Enter your shape color',
-        name: 'shapeColor',
+        name: 'inputShapeColor',
+        message: 'Enter your shape color keyword or hexadecimal number',
     }
 ]
 
-// Function to initialize app
-function init() {
+  // Create file
+  function createFile(answers){
+    Shape.setColor(answers.inputShapeColor);
+    console.log(Shape);
 
 }
+
+// Function to initialize app
+function init() {
+    return inquirer.prompt(questions)
+    .then((answers) => {
+      console.log(answers)
+      let userShape = answers.inputShape.toLowerCase()
+      switch(userShape) {
+        case "circle":
+          Shape = new Circle();
+          break;
+        case "triangle":
+          Shape = new Triangle();
+          break;
+        case "square":
+          Shape = new Square();
+            break;
+        default:
+          console.log("You have to choose circle, triangle or square")
+      }
+      console.log(Shape);
+      createFile(answers);
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  }
 
 // Function call to initialize app
 init();
